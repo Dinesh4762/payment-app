544c4fe2b37a132168d6d7f018e2345341b69eaf
Dinesh Kumar <kumar62georgian@gmail.com>
2024-04-21 11:08:16 +0530

    added vercel.json

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1",rootRouter);
 
-
+app.get("/", (req, res) => {
+    res.send("Hello, Bhai!");
+});
 app.listen(3000,()=>{
     console.log("server started!")
 }); 
