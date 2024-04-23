784c1adcdb88cf9620b971308623d3795466af73
Dinesh Kumar <kumar62georgian@gmail.com>
2024-04-23 17:00:45 +0530

    cors error

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const rootRouter = require("./routes/index")
+const rootRouter = require("./routes/index");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://paytm-app-basic.vercel.app",
+    methods: "GET,PUT,POST, DELETE,PATCH,HEAD",
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use("/api/v1",rootRouter);
+app.use("/api/v1", rootRouter);
 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello, Bhai!");
+  res.status(200).send("Hello, Bhai!");
 });
-app.listen(3000,()=>{
-    console.log("server started!")
-}); 
+app.listen(3000, () => {
+  console.log("server started!");
+});
